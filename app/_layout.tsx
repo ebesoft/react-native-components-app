e66d955dd9afafb9497bfeae019b8d461ee20c69
ebesoft <emersonyes3@gmail.com>
2025-01-11 15:10:37 -0500

    Nativewind y paleta de colores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,9 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { Text, View } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import "../global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,10 +30,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      
+      <View className='bg-light-background dark:bg-dark-background'>
+        <Text className='mt-10 text-3xl text-light-text dark:text-dark-text'>Hello</Text>
+      </View>
+      
+      {/*<Stack>
+      </Stack>*/}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
