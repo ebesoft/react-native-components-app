4ee28a0a69eeaadef80c63509f4a869394debf87
ebesoft <emersonyes3@gmail.com>
2025-01-11 15:43:20 -0500

    Personalizacion del View

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,12 +7,19 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { Text, View } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import "../global.css";
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const backgroundColor = useThemeColor({}, 'background');
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -29,15 +36,19 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      
-      <View className='bg-light-background dark:bg-dark-background'>
-        <Text className='mt-10 text-3xl text-light-text dark:text-dark-text'>Hello</Text>
-      </View>
-      
-      {/*<Stack>
-      </Stack>*/}
-      <StatusBar style="auto" />
-    </ThemeProvider>
+
+    <GestureHandlerRootView style={{ backgroundColor: 'purple', flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        
+        <ThemedView margin>
+          <Text className='mt-10 text-3xl text-light-text dark:text-dark-text'>Hello</Text>
+        </ThemedView>
+        
+        {/*<Stack>
+        </Stack>*/}
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
+    
   );
 }
